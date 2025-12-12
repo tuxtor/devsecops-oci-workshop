872235d88fd28fc3f6946cd4fd00d6c423b16f38
Victor Orozco <me@vorozco.com>
2025-12-12 20:36:24 -0300

    Spotbugs detection demo

--- quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
+++ quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
@@ -11,6 +11,12 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        var dummyVar = doSpotBugsDemo();
         return "Hello from Quarkus REST";
+    }
+
+    private String doSpotBugsDemo() {
+        var dummyVar = "This is not an error";
+        return "SpotBugs demo";
     }
 }
