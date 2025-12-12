d9598141f88c8f2ddc479b234f486f8fb6f292aa
Victor Orozco <me@vorozco.com>
2025-12-12 20:39:02 -0300

    Making spotbugs happy

--- quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
+++ quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
@@ -11,12 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        var dummyVar = doSpotBugsDemo();
         return "Hello from Quarkus REST";
-    }
-
-    private String doSpotBugsDemo() {
-        var dummyVar = "This is not an error";
-        return "SpotBugs demo";
     }
 }
